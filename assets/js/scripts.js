dc4f4b6c290c5a5c9b657e21ab5137cd93ae18cf
saulosilverio <saulossilverio@gmail.com>
2018-10-15 09:11:46 -0300

    ++ Divid

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 // NIVEL
 $('.star.rating').click(function(){
-  console.log( $(this).parent().data('stars') + ", " + $(this).data('rating'));
-  $(this).parent().attr('data-stars', $(this).data('rating'));
+	console.log( $(this).parent().data('stars') + ", " + $(this).data('rating'));
+	$(this).parent().attr('data-stars', $(this).data('rating'));
 });
