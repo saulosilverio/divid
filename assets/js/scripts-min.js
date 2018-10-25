c1cf7b64fb12e398b07461d388104d10692ac6da
saulosilverio <saulossilverio@gmail.com>
2018-10-25 08:55:35 -0200

    ++ Swipe to delete

--- assets/js/scripts-min.js
+++ assets/js/scripts-min.js
@@ -1,1 +1,1 @@
-$(".star.rating").click(function(){console.log($(this).parent().data("stars")+", "+$(this).data("rating")),$(this).parent().attr("data-stars",$(this).data("rating"))});
+$(".star.rating").click(function(){console.log($(this).parent().data("stars")+", "+$(this).data("rating")),$(this).parent().attr("data-stars",$(this).data("rating"))}),new SwipeOut(document.getElementById("list")),$("#list").on("click","li",function(t){}).on("delete","li",function(t){$(this).addClass("delete")});
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,14 @@ $('.star.rating').click(function(){
 	console.log( $(this).parent().data('stars') + ", " + $(this).data('rating'));
 	$(this).parent().attr('data-stars', $(this).data('rating'));
 });
+
+// LISTA
+new SwipeOut(document.getElementById("list"));
+
+// DELETE
+$("#list").on("click", "li", function(e) {
+	// editar jogador
+}).on("delete", "li", function(e) {
+	$(this).addClass("delete");
+});
+    
