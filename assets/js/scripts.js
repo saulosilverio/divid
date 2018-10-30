e9af71da1d5e07bf8a72eca968ef516b4d4d99dc
saulosilverio <saulossilverio@gmail.com>
2018-10-30 09:09:27 -0200

    ++ Dvid

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,30 @@ $('.star.rating').click(function(){
 });
 
 // LISTA
-$('#jogador').on("swipeleft", function(){
-	$(this).addClass('left');
+$(function(){
+  $('#list > li').on('swipeleft', swipeleftHandler);
+  $('#list > li').on('swiperight', swiperightHandler);
+ 
+  function swipeleftHandler(event){
+	event.preventDefault();
+    $(event.target).addClass('show-remove');
+  }
+  function swiperightHandler(event){
+	event.preventDefault();
+    $(event.target).removeClass('show-remove');
+  }
 });
+
+// REMOVE LINHA
+$('.btn-remove-lista').click(removeLinha);
+
+function removeLinha() {
+    event.preventDefault();
+    var linha = $(this).parent();
+
+    linha.fadeOut(1000);
+    setTimeout(function() {
+        linha.remove();
+    }, 1000);
+}
     
