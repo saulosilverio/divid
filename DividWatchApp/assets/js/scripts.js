52d4a9fb785648c2c66aaadcdac9bd17e2c841d8
saulosilverio <saulossilverio@gmail.com>
2018-12-04 09:34:47 -0200

    ++ Projeto do CodeKit

---
+++ DividWatchApp/assets/js/scripts.js
@@ -0,0 +1,36 @@
+// NIVEL
+$('.star.rating').click(function(){
+	//console.log( $(this).parent().data('stars') + ", " + $(this).data('rating'));
+	$(this).parent().attr('data-stars', $(this).data('rating'));
+});
+
+// LISTA
+$(function(){
+  $('#list > li').on('swipeleft', swipeleftHandler);
+  $('#list > li').on('swiperight', swiperightHandler);
+ 
+  function swipeleftHandler(event){
+	event.preventDefault();
+    $(event.target).addClass('show-remove');
+  }
+  function swiperightHandler(event){
+	event.preventDefault();
+    $(event.target).removeClass('show-remove');
+  }
+});
+
+
+// REMOVE LINHA
+$('.btn-remove-lista').click(removeLinha);
+
+function removeLinha() {
+    event.preventDefault();
+    var linha = $(this).parent();
+
+    linha.fadeOut(1000);
+    setTimeout(function() {
+        linha.remove();
+    }, 1000);
+}
+
+    
